445f16dcecfbfcdab20808bb921c1e5ce44b2c73
b0ssplz <marcative@gmail.com>
2025-02-05 12:56:31 +0100

    config added

--- Task 2/ardigen_2_main.py
+++ Task 2/ardigen_2_main.py
@@ -1,24 +1,32 @@
-import csv
-from typing import List, Dict, Tuple
-from file_operations.data_loader import DataLoader
 from converters.currency_converter import CurrencyConverter
 from data_processing.data_processor import DataProcessor
+from file_operations.data_loader import DataLoader
 from file_operations.result_saver import ResultSaver
+from config.config import Config
+import os
 
 
 def main():
     """Main function to load data, process it, and save the results."""
-    data = DataLoader.load_csv("data.csv")
-    currencies = DataLoader.load_csv("currencies.csv")
-    matchings = DataLoader.load_csv("matchings.csv")
+    data = DataLoader.load_csv(os.path.join(Config.INPUT_FILE_FOLDER, Config.DATA))
+    currencies = DataLoader.load_csv(
+        os.path.join(Config.INPUT_FILE_FOLDER, Config.CURRENCIES)
+    )
+    matchings = DataLoader.load_csv(
+        os.path.join(Config.INPUT_FILE_FOLDER, Config.MATCHINGS)
+    )
 
     currency_converter = CurrencyConverter(currencies)
 
     for row in data:
-        row['price_pln'] = currency_converter.convert_to_pln(float(row['price']), row['currency'])
+        row["price_pln"] = currency_converter.convert_to_pln(
+            float(row["price"]), row["currency"]
+        )
 
     results = DataProcessor.process_data(data, matchings)
-    ResultSaver.save_results(results, "top_products.csv")
+    ResultSaver.save_results(
+        results, (os.path.join(Config.OUTPUT_FILE_FOLDER, Config.TOP_PRODUCTS))
+    )
 
 
 if __name__ == "__main__":
---
+++ Task 2/config/config.py
@@ -0,0 +1,14 @@
+class Config():
+
+    # Folders
+    INPUT_FILE_FOLDER = 'Task 2/input_files'
+    OUTPUT_FILE_FOLDER = 'Task 2/output_files'
+
+
+    # Input
+    CURRENCIES = 'currencies.csv'
+    DATA = 'data.csv'
+    MATCHINGS = 'matchings.csv'
+
+    # Output
+    TOP_PRODUCTS = 'top_products.csv'
